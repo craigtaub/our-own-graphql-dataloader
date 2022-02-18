0a2fbb98ebe42280b125711a1e9eaf8199a207bf
Craig Taub <craigtaub@gmail.com>
2022-02-18 21:01:45 +0000

    batch n prom

--- mine/batch-and-promise.js
+++ mine/batch-and-promise.js
@@ -1,30 +1,45 @@
-// TODO
+// calls batch with both keys. returns values from batch function
 
 let resolvedPromise;
 let batchFunction;
-const keys = [];
-
+let batch = {
+  keys: [],
+  callbacks: [],
+};
 const dispatchBatch = () => {
-  batchFunction(keys);
+  const batchPromise = batchFunction(batch.keys);
+
+  batchPromise.then((values) => {
+    // returned from batch function
+    for (var i = 0; i < batch.callbacks.length; i++) {
+      var value = values[i];
+      batch.callbacks[i].resolve(value);
+    }
+  });
 };
 const batchScheduleFn = (cb) => {
   const resolvedPromise = Promise.resolve();
   resolvedPromise.then(() => {
-    process.nextTick(cb); // called twice. [1,2], [1,2]
-    // cb(); // called twice. [1], [1,2]
+    process.nextTick(cb);
   });
 };
 
 const load = async (id) => {
-  keys.push(id);
+  batch.keys.push(id);
   batchScheduleFn(() => {
     dispatchBatch();
   });
+  const promise = new Promise((resolve, reject) => {
+    batch.callbacks.push({ resolve, reject });
+  });
+
+  return promise;
 };
 
 const run = async () => {
-  batchFunction = (keys) => {
+  batchFunction = async (keys) => {
     console.log("BATCH, keys:", keys);
+    return keys.map((key) => ({ id: key }));
   };
   const a = await load(1);
   const b = await load(2);
