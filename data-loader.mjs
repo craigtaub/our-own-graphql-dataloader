0e7aa99d20837faadb262e14dce072c59de690db
Craig Taub <craigtaub@gmail.com>
2022-02-18 15:40:20 +0000

    add call-stack notes

--- data-loader.mjs
+++ data-loader.mjs
@@ -8,28 +8,36 @@ export class DataLoader {
   //   return JSON.stringify(key);
   // }
   _batchScheduleFn(fn) {
+    console.log('[data-loader] - _batchScheduleFn - pre')
     if (!resolvedPromise) {
+      console.log('[data-loader] - _batchScheduleFn - set resolvedPromise')
       resolvedPromise = Promise.resolve();
     }
+    console.log('[data-loader] - _batchScheduleFn - resolvedPromise called')
     resolvedPromise.then(() => {
+      console.log('[data-loader] - _batchScheduleFn - resolvedPromise.then nextTick')
       process.nextTick(fn);
     });
   }
   _dispatchBatch() {
+    console.log('[data-loader] - _dispatchBatch - pre')
     this._batch.hasDispatched = true;
 
     // If there's nothing to load, resolve any cache hits and return early.
-    if (this._batch.keys.length === 0) {
-      // resolveCacheHits(batch);
-      return;
-    }
+    // if (this._batch.keys.length === 0) {
+    //   resolveCacheHits(batch);
+    //   return;
+    // }
+
     // Call the provided batchLoadFn for this loader with the batch's keys and
     // with the loader as the `this` context.
     const batchPromise = this._batchLoadFn(this._batch.keys);
 
+    console.log('[data-loader] - _dispatchBatch - load batch fn')
     // Await the resolution of the call to batchLoadFn.
     batchPromise
       .then((values) => {
+        console.log('[data-loader] - _dispatchBatch - then')
         // Resolve all cache hits in the same micro-task as freshly loaded values.
         // resolveCacheHits(batch);
 
@@ -39,6 +47,7 @@ export class DataLoader {
           if (value instanceof Error) {
             this._batch.callbacks[i].reject(value);
           } else {
+            console.log('[data-loader] - _dispatchBatch - resolve')
             this._batch.callbacks[i].resolve(value);
           }
         }
@@ -48,34 +57,31 @@ export class DataLoader {
       });
   }
 
-  // do not cache individual loads if the entire batch dispatch fails,
-  // but still reject each request so they do not hang.
-  _failedDispatch(error) {
-    // Cache hits are resolved, even though the batch failed.
-    // resolveCacheHits(batch);
-    for (var i = 0; i < this._batch.keys.length; i++) {
-      this.clear(this._batch.keys[i]);
-      this._batch.callbacks[i].reject(error);
-    }
-  }
   _getCurrentBatch() {
+    console.log('[data-loader] - _getCurrentBatch - pre')
     const existingBatch = this._batch;
     // existing batch which has not yet dispatched
     if (existingBatch && !existingBatch.hasDispatched) {
+      console.log('[data-loader] - _getCurrentBatch - return existing batch')
       return existingBatch;
     }
+    console.log('[data-loader] - _getCurrentBatch - no existing batch')
 
     // new batch
     const newBatch = { hasDispatched: false, keys: [], callbacks: [] };
     this._batch = newBatch;
     // schedule a task to dispatch this batch of requests.
+    console.log('[data-loader] - _getCurrentBatch - _batchScheduleFn')
     this._batchScheduleFn(() => {
+      console.log('[data-loader] - _getCurrentBatch - _dispatchBatch')
       this._dispatchBatch();
     });
+    console.log('[data-loader] - _getCurrentBatch - return new batch')
 
     return newBatch;
   }
   async load(key) {
+    console.log('[data-loader] - load - pre')
     // const batch = { keys: [], callbacks: [] }; // TODO - current batch
     const batch = this._getCurrentBatch();
 
@@ -83,6 +89,7 @@ export class DataLoader {
 
     batch.keys.push(key);
     const promise = new Promise((resolve, reject) => {
+      console.log('[data-loader] - load - batch.callback push prom')
       batch.callbacks.push({ resolve, reject });
     });
 
@@ -90,27 +97,39 @@ export class DataLoader {
     // const cacheKey = this._cacheKeyFn(key);
     // this._cacheMap.set(cacheKey, promise);
 
+    console.log('[data-loader] - load - return')
     return promise;
   }
 
-  resolveCacheHits(batch) {
-    // Resolves the Promises for any cache hits in this batch.
-    if (batch.cacheHits) {
-      for (var i = 0; i < batch.cacheHits.length; i++) {
-        batch.cacheHits[i]();
-      }
-    }
-  }
+  // resolveCacheHits(batch) {
+  //   // Resolves the Promises for any cache hits in this batch.
+  //   if (batch.cacheHits) {
+  //     for (var i = 0; i < batch.cacheHits.length; i++) {
+  //       batch.cacheHits[i]();
+  //     }
+  //   }
+  // }
 
-  // Clears the value at `key` from the cache
-  clear(key) {
-    // var cacheMap = this._cacheMap;
-    // if (cacheMap) {
-    // var cacheKey = this._cacheKeyFn(key);
-    // cacheMap.delete(cacheKey);
-    // }
-    return this;
-  }
+  // // do not cache individual loads if the entire batch dispatch fails,
+  // // but still reject each request so they do not hang.
+  // _failedDispatch(error) {
+  //   // Cache hits are resolved, even though the batch failed.
+  //   // resolveCacheHits(batch);
+  //   for (var i = 0; i < this._batch.keys.length; i++) {
+  //     this.clear(this._batch.keys[i]);
+  //     this._batch.callbacks[i].reject(error);
+  //   }
+  // }
+
+  // // Clears the value at `key` from the cache
+  // clear(key) {
+  //   // var cacheMap = this._cacheMap;
+  //   // if (cacheMap) {
+  //   // var cacheKey = this._cacheKeyFn(key);
+  //   // cacheMap.delete(cacheKey);
+  //   // }
+  //   return this;
+  // }
 }
 
 // Private: cached resolved Promise instance
--- index.mjs
+++ index.mjs
@@ -1,30 +1,40 @@
 import { DataLoader } from "./data-loader.mjs";
 
 const database = {
+  0: {
+    id: 0,
+    name: "zero",
+    lastInvitedID: 3,
+    invitedByID: 3,
+  },
   1: {
     id: 1,
     name: "one",
-    lastInvitedID: "0",
-    invitedByID: "0",
+    lastInvitedID: 0,
+    invitedByID: 0,
   },
   2: {
     id: 2,
     name: "two",
-    lastInvitedID: "1",
-    invitedByID: "1",
+    lastInvitedID: 1,
+    invitedByID: 1,
   },
 };
 async function threadTwo(loader) {
   // Elsewhere in your application
   const user = await loader.load(2);
-  const lastInvited = await loader.load(user.lastInvitedID);
-  console.log(`User 2 last invited ${lastInvited}`);
+  console.log('threadTwo user', user.id)
+  // simplified
+  // const lastInvited = await loader.load(user.lastInvitedID);
+  // console.log(`User 2 last invited ${lastInvited.name}`);
 }
 
 async function threadOne(loader) {
   const user = await loader.load(1);
-  const invitedBy = await loader.load(user.invitedByID);
-  console.log(`User 1 was invited by ${invitedBy}`);
+  console.log('threadOne user', user.id)
+  // simplified
+  // const userInvitedBy = await loader.load(user.invitedByID);
+  // console.log(`User 1 was invited by ${userInvitedBy.name}`);
 }
 
 const run = async () => {
@@ -33,13 +43,16 @@ const run = async () => {
     // Query by id those missing
     // Set response values key and value into cache (key == id and edge)
 
-    // console.log("batch function keys", keys);
+    console.log("batch function keys", keys);
     const values = keys.map((key) => {
       return database[key];
     });
     return values;
   });
 
+  // await means batch func called with individual keys
+  // WHY - as the batched keys arent both threadOne, its threadOne + threadTwo sync
+  // SO loader called with first load(x) of each sync function
   threadOne(loader);
   threadTwo(loader);
 };
