c7f5bc845ed8293e14590dae14030051511c6ea9
Craig Taub <craigtaub@gmail.com>
2022-02-19 10:50:18 +0000

    split in 2

--- mine/batch-and-scheduler.mjs
+++ mine/batch-and-scheduler.mjs
@@ -1,15 +1,24 @@
-// calls batch twice with both keys. but doesnt return
+// calls batch twice with both keys. but doesnt return. uses batchSchedule functions
 
+let resolvedPromise;
 let batchFunction;
 const keys = [];
 
-const load = async (id) => {
-  keys.push(id);
+const dispatchBatch = () => {
+  batchFunction(keys);
+};
+
+const batchScheduleFn = (cb) => {
   const resolvedPromise = Promise.resolve();
   resolvedPromise.then(() => {
-    process.nextTick(() => {
-      batchFunction(keys);
-    });
+    process.nextTick(cb);
+  });
+};
+
+const load = async (id) => {
+  keys.push(id);
+  batchScheduleFn(() => {
+    dispatchBatch();
   });
 };
 
